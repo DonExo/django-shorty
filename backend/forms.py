a78e8ec90426d397e3d0e0c4b55d3f22e2f5c407
Dimitriovski <d.dimitriovski@ultimaker.com>
2020-03-30 23:03:42 +0200

    Added placeholder in the URL field

--- backend/forms.py
+++ backend/forms.py
@@ -3,6 +3,8 @@ from .models import Shortener as ModelShortener
 
 
 class FormShortener(forms.ModelForm):
+    original_url = forms.URLField(label="URL Field", widget=forms.TextInput(attrs={'placeholder': 'Your LOOOONG url here'}))
+
     class Meta:
         model = ModelShortener
         fields = ('original_url', )
